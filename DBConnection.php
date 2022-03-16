7819c2019905ba9dba6ea75f2d246ef28a9460c6
AhdKarman <karahdman1@gmail.com>
2022-03-16 16:53:18 +0300

    Select Query

--- DBConnection.php
+++ DBConnection.php
@@ -4,16 +4,39 @@ private $dsn ="mysql:host=localhost;dbname=queryBuilder" ;
 private $username = "root";
 private $password = "";
 
-private $pdo;
+private $conn;
 private $finalQuery;
 
 function __construtor(){
 
-    $this->pdo = new PDO($this-> dsn , $this-> username , $this-> password) ;
+    $this->conn = new PDO($this-> dsn , $this-> username , $this-> password) ;
 }
 
-function select(){
+function select($table_name , array $columns = ["*"]){
+    $col_names = implode("," , $columns);
+    $query = "SELECT $col_names FROM $table_name ";
+    $this->finalQuery = $query ;
 
+    runQuery($query);
+
+
+    
+    return $this;
+}
+
+function where(array $conditions = null){
+    $where_condition = "";
+
+    if ($conditions) {
+      foreach ($conditions as $item) 
+      {
+          $query;
+          // Rest will be here ;
+      }
+    }
+    else{
+        $query= $this->finalQuery;
+    }
     return $this;
 }
 
@@ -32,8 +55,6 @@ function delete (){
 
     return $this;
 }
-
-/* Challenge Requirements */
 
 function orderBy(){
 
@@ -65,18 +86,15 @@ function limit(){
     return $this;
 }
 
-function where(){
-    return $this;
-}
 
 function orWhere(){
 
     return $this;
 }
 
-function runQuery(){
-    $stm = $this->pdo->prepare($this->finalQuery);
-    $stm->execute([]);
+function runQuery($query){
+    $stm = $this->conn->prepare($this->finalQuery);
+$stm->execute(/*[]*/); // here you can pass columns
     return $stm->fetchAll(PDO::FETCH_OBJ);
 }
 
